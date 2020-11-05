6e05eaee81f994fd67ee4dc2c40b28229a223061
Diego Ordóñez <diego.ordonez@gmail.com>
2020-11-05 09:30:57 -0500

    Update

--- src/udp/Client.java
+++ src/udp/Client.java
@@ -6,7 +6,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 /**
- *
+ * https://github.com/dordonez-ute-apdist/udp
  * @author dordonez@ute.edu.ec
  */
 public class Client {
@@ -16,21 +16,28 @@ public class Client {
                 
         //Crea el socket cliente
         DatagramSocket socket = new DatagramSocket();
-        //Obtiene la IP del servidor
-        InetAddress direccion = InetAddress.getByName(Server.DIRECCION); 
+        System.out.println("El cliente se reserva el puerto: " + socket.getLocalPort());
+        
+        //Define la dirección(IP o nombre) del servidor
+        InetAddress direccion = InetAddress.getByName(Server.DIRECCION);
+        
         //Mensajes a enviar
-        String[] mensajes = {"HOLA", "Primer mensaje", "Segundo mensaje", "CHAO", Server.END_OF_MSGS};
+        String[] mensajes = {"HOLA", "Primer mensaje", "Segundo mensaje", "CHAO", Server.MSJ_FIN};
 
         for(String msj : mensajes) {
             //Convierte el valor a byte[]
             byte[] buffer = msj.getBytes();
-            //Crea el datagrama a enviar con el valor, y la direcciÃ³n y puerto del servidor
+            
+            //Crea el datagrama a enviar con el valor, y la dirección y puerto del servidor
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, Server.PUERTO);
-            //envÃ­a el datagrama
+            
+            //envía el datagrama
             socket.send(packet);
-            //presenta la informaciÃ³n enviada
+            
+            //presenta la información enviada
             System.out.println(
-                String.format("Enviado: %s; A: %s", msj, packet.getSocketAddress()));
+                String.format("Enviado: %s; hacia: %s", new String(packet.getData()), packet.getSocketAddress()));
+            
             //Pausa el trabajo del hilo
             Thread.sleep(3000);
         }
--- src/udp/Server.java
+++ src/udp/Server.java
@@ -5,36 +5,42 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 /**
- *
+ * https://github.com/dordonez-ute-apdist/udp
  * @author dordonez@ute.edu.ec
  */
 public class Server {
     public static final String DIRECCION  = "localhost";
     public static final int PUERTO = 9595;
     public static final int TALLA_BUFFER = 256;
-    public static final String END_OF_MSGS  = "EXIT";
+    public static final String MSJ_FIN  = "EXIT";
 
     public static void main(String[] args) throws Exception {
+        System.out.println("UDP SERVER");
+        
         //Crea el socket servidor
         DatagramSocket socket = new DatagramSocket(Server.PUERTO);
-        byte[] buffer;
-        System.out.println("UDP SERVER");
+        System.out.println("El servidor se reserva el puerto: " + socket.getLocalPort());
+        
         while(true) {
             //Crea el buffer para guardar los datos que se reciban
-            buffer = new byte[Server.TALLA_BUFFER];
+        	byte[] buffer = new byte[Server.TALLA_BUFFER];
+        	
             //Crea un datagrama con el buffer vacío para recibir los datos
             DatagramPacket packet = new DatagramPacket(buffer, Server.TALLA_BUFFER);
+            
             //Espera que llegue un datagrama y lo copia en el datagrama creado
             socket.receive(packet);
-            System.out.println("XYZ");
+            
             //presenta la infomación recibida
             String carga = new String(packet.getData()).trim();
             System.out.println(
-                String.format("Recibido: %s; Desde: %s", carga, packet.getSocketAddress()));
+                String.format("Recibido: %s; desde: %s", carga, packet.getSocketAddress()));
 
-            if(carga.equals(Server.END_OF_MSGS)) break;
+            //si recibe el mensaje "EXIT" finaliza
+            if(carga.equals(Server.MSJ_FIN)) break;
         }
         
+        //cierra el socket
         socket.close();
     }
     
